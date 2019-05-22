12fe9e6babd79200dc4fb1f9044e69ce2d489eeb
MAciej <maciej.jancewicz@gmail.com>
2019-05-22 15:49:16 +0200

    def draw_launchpad_board

--- launchpad.py
+++ launchpad.py
@@ -21,7 +21,35 @@ def translate_board_position():
   pass
 
 def draw_launchpad_board():
-  pass
+	midi_port.send_message([0x90, 2, 27])
+	midi_port.send_message([0x90, 5, 27])
+	midi_port.send_message([0x90, 18, 27])
+	midi_port.send_message([0x90, 21, 27])
+	midi_port.send_message([0x90, 32, 27])
+	midi_port.send_message([0x90, 33, 27])
+	midi_port.send_message([0x90, 34, 27])
+	midi_port.send_message([0x90, 35, 27])
+	midi_port.send_message([0x90, 36, 27])
+	midi_port.send_message([0x90, 37, 27])
+	midi_port.send_message([0x90, 38, 27])
+	midi_port.send_message([0x90, 39, 27])
+	midi_port.send_message([0x90, 50, 27])
+	midi_port.send_message([0x90, 53, 27])
+	midi_port.send_message([0x90, 66, 27])
+	midi_port.send_message([0x90, 69, 27])
+	midi_port.send_message([0x90, 80, 27])
+	midi_port.send_message([0x90, 81, 27])
+	midi_port.send_message([0x90, 82, 27])
+	midi_port.send_message([0x90, 83, 27])
+	midi_port.send_message([0x90, 84, 27])
+	midi_port.send_message([0x90, 85, 27])
+	midi_port.send_message([0x90, 86, 27])
+	midi_port.send_message([0x90, 87, 27])
+	midi_port.send_message([0x90, 98, 27])
+	midi_port.send_message([0x90, 101, 27])
+	midi_port.send_message([0x90, 114, 27])
+	midi_port.send_message([0x90, 117, 27])
+
 
 def get_pushed_button_coords():
   print("not needed now")
