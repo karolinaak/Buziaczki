563d9461fb084fd74911acbc5e8e51285e0b7a00
mj <maj1337+git@gmail.com>
2019-05-22 14:59:52 +0200

    connect to device

--- launchpad.py
+++ launchpad.py
@@ -6,8 +6,15 @@ import rtmidi
 COLOR_X = <something>
 COLOR_O = <something>
 
-def connect_to_device():
-  pass
+def connect_to_device(which_port='Launchpad Mini'):
+  mo = rtmidi.MidiOut()
+    for port_no in range(mo.get_port_count()):
+            port_name = mo.get_port_name(port_no)
+            print("MIDI out:", port_name)
+            if port_name.find(which_port) > -1: 
+                # or 'Launchpad Pro Standalone Port'
+                midi_port = mo.open_port(port_no)
+                return midi_port
 
 # change x, y -> launchpad_position
 def translate_board_position():
