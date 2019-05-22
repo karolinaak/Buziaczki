f841f3152f1d473d4c35af5b96f313e2d0c00690
mj <maj1337+git@gmail.com>
2019-05-22 14:25:16 +0200

    launchpad starter

---
+++ launchpad.py
@@ -0,0 +1,21 @@
+import rtmidi
+
+COLOR_X = <something>
+COLOR_O = <something>
+
+def connect_to_device():
+  pass
+
+# change x, y -> launchpad_position
+def translate_board_position():
+  pass
+
+def draw_launchpad_board():
+  pass
+
+def get_pushed_button_coords():
+  print("not needed now")
+  pass
+
+def turn_on_pixel():
+  pass
