6609aad7af658e152cb2401052ad17053c5c7dc9
apacek <pacyreg@gmail.com>
2019-06-06 10:23:48 +0200

    1st tets with nbp api

---
+++ ap_nbp_1.py
@@ -0,0 +1,13 @@
+import requests
+
+resp = requests.get("http://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=json")
+result = resp.json()
+#print(result)
+def show_me_gbp():
+    index = 0
+    for i in result:
+        #print(result['currency'][i])
+        print(result['rates'][index]['no']," = ", result['rates'][index]['mid'])
+        index = index + 1
+
+show_me_gbp()
